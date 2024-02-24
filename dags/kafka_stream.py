53a4e3e4cdfb25d0d3d065d215d87e0f8135934a
babuush <thanapatpiyamassakul@gmail.com>
2024-02-24 18:23:53 +0700

    Create docker-compose file

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -14,13 +14,30 @@ def get_data():
     res = res.json()
     res = res['results'][0]
     return res
-def format_data():
-    return
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['postcode'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
 def stream_data():
     import json
-    
-    
-    
+
+    res = get_data()
+    res = format_data(res)
     
     print(json.dumps(res, indent=3))
 
