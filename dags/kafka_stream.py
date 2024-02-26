be5c6a91c31ea88b88d4563ecbf8ace26b47d8bc
babuush <thanapatpiyamassakul@gmail.com>
2024-02-26 16:01:04 +0700

    Update kafka_stream.py

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -35,12 +35,16 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
+    import time
 
     res = get_data()
     res = format_data(res)
-    
-    print(json.dumps(res, indent=3))
+    # print(json.dumps(res, indent=3))
 
+    producer = KadkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer.send('user_created', json.dumps(res).encode('utf-8'))
+                             
 with DAG('user_automation', 
          default_args=defualt_args,
          schedule='@daily',
