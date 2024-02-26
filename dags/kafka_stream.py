7b08c535a21cb6b394ecbcf0184474545d8970e5
babuush <thanapatpiyamassakul@gmail.com>
2024-02-26 16:44:10 +0700

    Update gitignore And update kafka_stream

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -42,7 +42,7 @@ def stream_data():
     res = format_data(res)
     # print(json.dumps(res, indent=3))
 
-    producer = KadkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'], max_block_ms=5000)
     producer.send('user_created', json.dumps(res).encode('utf-8'))
                              
 with DAG('user_automation', 
